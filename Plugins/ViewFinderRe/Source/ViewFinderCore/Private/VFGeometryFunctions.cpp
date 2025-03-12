f92f8d9169b35a9a41c6aa5caf448fe8f742508e
StrangeDS <1023221456@qq.com>
2025-03-13 01:35:57 +0800

    UVFGeometryFunctions: 增加宏 WITHOUT_GEOMETRY_SCRIPT 参数传递并还原插件调用 另预留宏 MANUAL_CONVERT
    禁用报错也有点麻烦, 留给后人吧
    累死我了 /(ㄒoㄒ)/~~

--- Plugins/ViewFinderRe/Source/ViewFinderCore/Private/VFGeometryFunctions.cpp
+++ Plugins/ViewFinderRe/Source/ViewFinderCore/Private/VFGeometryFunctions.cpp
@@ -1,5 +1,12 @@
 #include "VFGeometryFunctions.h"
 
+// WITHOUT_GEOMETRY_SCRIPT开启后(默认), 将不依赖插件.
+// 关闭宏, 将依赖插件接口(考虑到后续更新)
+#define WITHOUT_GEOMETRY_SCRIPT 1
+
+#if WITHOUT_GEOMETRY_SCRIPT
+// 脱离插件使用
+
 #include "DynamicMesh/DynamicMesh3.h"
 #include "DynamicMesh/MeshTransforms.h"
 #include "UDynamicMesh.h"
@@ -196,8 +203,8 @@ UDynamicMesh *UVFGeometryFunctions::CopyMeshFromStaticMesh(
 
 	int32 UseLODIndex = FMath::Clamp(RequestedLOD.LODIndex, 0, FromStaticMeshAsset->GetNumLODs() - 1);
 
-	const FStaticMeshLODResources* LODResources = nullptr;
-	if (FStaticMeshRenderData* RenderData = FromStaticMeshAsset->GetRenderData())
+	const FStaticMeshLODResources *LODResources = nullptr;
+	if (FStaticMeshRenderData *RenderData = FromStaticMeshAsset->GetRenderData())
 	{
 		LODResources = &RenderData->LODResources[UseLODIndex];
 	}
@@ -206,7 +213,7 @@ UDynamicMesh *UVFGeometryFunctions::CopyMeshFromStaticMesh(
 	FStaticMeshLODResourcesToDynamicMesh::ConversionOptions ConvertOptions;
 #if WITH_EDITOR
 	// respect BuildScale build setting
-	const FMeshBuildSettings& LODBuildSettings = FromStaticMeshAsset->GetSourceModel(UseLODIndex).BuildSettings;
+	const FMeshBuildSettings &LODBuildSettings = FromStaticMeshAsset->GetSourceModel(UseLODIndex).BuildSettings;
 	ConvertOptions.BuildScale = (FVector3d)LODBuildSettings.BuildScale3D;
 #endif
 
@@ -472,10 +479,10 @@ UDynamicMesh *UVFGeometryFunctions::CopyMeshFromComponent(
 			bSuccess = true;
 		}
 	}
-	else if (UStaticMeshComponent* StaticMeshComponent = Cast<UStaticMeshComponent>(Component))
+	else if (UStaticMeshComponent *StaticMeshComponent = Cast<UStaticMeshComponent>(Component))
 	{
 		LocalToWorld = StaticMeshComponent->GetComponentTransform();
-		UStaticMesh* StaticMesh = StaticMeshComponent->GetStaticMesh();
+		UStaticMesh *StaticMesh = StaticMeshComponent->GetStaticMesh();
 		if (ensure(StaticMesh))
 		{
 			FVF_GeometryScriptCopyMeshFromAssetOptions AssetOptions;
@@ -485,11 +492,12 @@ UDynamicMesh *UVFGeometryFunctions::CopyMeshFromComponent(
 				StaticMesh, ToDynamicMesh, AssetOptions, Options.RequestedLOD);
 
 			// if we have an ISMC, append instances
-			if (UInstancedStaticMeshComponent* ISMComponent = Cast<UInstancedStaticMeshComponent>(StaticMeshComponent))
+			if (UInstancedStaticMeshComponent *ISMComponent = Cast<UInstancedStaticMeshComponent>(StaticMeshComponent))
 			{
 				FDynamicMesh3 InstancedMesh;
-				ToDynamicMesh->EditMesh([&](FDynamicMesh3& EditMesh) { InstancedMesh = MoveTemp(EditMesh); EditMesh.Clear(); },
-					EDynamicMeshChangeType::MeshChange, EDynamicMeshAttributeChangeFlags::Unknown, true);
+				ToDynamicMesh->EditMesh([&](FDynamicMesh3 &EditMesh)
+										{ InstancedMesh = MoveTemp(EditMesh); EditMesh.Clear(); },
+										EDynamicMeshChangeType::MeshChange, EDynamicMeshAttributeChangeFlags::Unknown, true);
 
 				FDynamicMesh3 AccumMesh;
 				AccumMesh.EnableMatchingAttributes(InstancedMesh);
@@ -506,32 +514,31 @@ UDynamicMesh *UVFGeometryFunctions::CopyMeshFromComponent(
 						FTransformSRT3d XForm(InstanceTransform);
 
 						Mappings.Reset();
-						Editor.AppendMesh(&InstancedMesh, Mappings,
-							[&](int, const FVector3d& Position) { return XForm.TransformPosition(Position); },
-							[&](int, const FVector3d& Normal) { return XForm.TransformNormal(Normal); });
+						Editor.AppendMesh(&InstancedMesh, Mappings, [&](int, const FVector3d &Position)
+										  { return XForm.TransformPosition(Position); }, [&](int, const FVector3d &Normal)
+										  { return XForm.TransformNormal(Normal); });
 					}
 				}
 
-				ToDynamicMesh->EditMesh([&](FDynamicMesh3& EditMesh) { EditMesh = MoveTemp(AccumMesh); },
-					EDynamicMeshChangeType::MeshChange, EDynamicMeshAttributeChangeFlags::Unknown, true);
+				ToDynamicMesh->EditMesh([&](FDynamicMesh3 &EditMesh)
+										{ EditMesh = MoveTemp(AccumMesh); },
+										EDynamicMeshChangeType::MeshChange, EDynamicMeshAttributeChangeFlags::Unknown, true);
 			}
 			bSuccess = true;
 		}
 	}
-	else if (UDynamicMeshComponent* DynamicMeshComponent = Cast<UDynamicMeshComponent>(Component))
+	else if (UDynamicMeshComponent *DynamicMeshComponent = Cast<UDynamicMeshComponent>(Component))
 	{
 		LocalToWorld = DynamicMeshComponent->GetComponentTransform();
-		UDynamicMesh* CopyDynamicMesh = DynamicMeshComponent->GetDynamicMesh();
+		UDynamicMesh *CopyDynamicMesh = DynamicMeshComponent->GetDynamicMesh();
 		if (ensure(CopyDynamicMesh))
 		{
-			CopyDynamicMesh->ProcessMesh([&](const FDynamicMesh3& Mesh)
-			{
-				ToDynamicMesh->SetMesh(Mesh);
-			});
+			CopyDynamicMesh->ProcessMesh([&](const FDynamicMesh3 &Mesh)
+										 { ToDynamicMesh->SetMesh(Mesh); });
 			bSuccess = true;
 		}
 	}
-	else if (UBrushComponent* BrushComponent = Cast<UBrushComponent>(Component))
+	else if (UBrushComponent *BrushComponent = Cast<UBrushComponent>(Component))
 	{
 		LocalToWorld = BrushComponent->GetComponentTransform();
 
@@ -548,7 +555,7 @@ UDynamicMesh *UVFGeometryFunctions::CopyMeshFromComponent(
 		ConvertedMesh.EnableAttributes();
 		if (Options.bWantNormals)
 		{
-			FDynamicMeshNormalOverlay* Normals = ConvertedMesh.Attributes()->PrimaryNormals();
+			FDynamicMeshNormalOverlay *Normals = ConvertedMesh.Attributes()->PrimaryNormals();
 			FMeshNormals::InitializeOverlayTopologyFromFaceGroups(&ConvertedMesh, Normals);
 			FMeshNormals::QuickRecomputeOverlayNormals(ConvertedMesh);
 		}
@@ -571,3 +578,411 @@ UDynamicMesh *UVFGeometryFunctions::CopyMeshFromComponent(
 }
 
 #undef LOCTEXT_NAMESPACE
+
+#else
+// 使用插件
+// 布尔操作经常报错, 但没关系, It Works Fine.
+// 记得其中一个原因是: 判断bool后模型的顶点数, 为0就报错.这种情况在我们这当然有, 而且很常见.
+
+#include "GeometryScript/CollisionFunctions.h"
+#include "GeometryScript/MeshAssetFunctions.h"
+#include "GeometryScript/MeshBasicEditFunctions.h"
+#include "GeometryScript/MeshBooleanFunctions.h"
+#include "GeometryScript/MeshPrimitiveFunctions.h"
+#include "GeometryScript/SceneUtilityFunctions.h"
+
+// 中间层参数需要转换为插件中的参数类型
+// 可考虑使用地址, memcpy(&b, &a, sizeof(A)), 但万一插件更新, 又新添了一个值呢?
+// 手动转换的健壮性强得多, 但也保留宏开关
+#define MANUAL_CONVERT 1
+
+#ifndef MANUAL_CONVERT
+#else
+
+static EGeometryScriptCollisionGenerationMethod Convert(
+	const EVF_GeometryScriptCollisionGenerationMethod &Meth)
+{
+	EGeometryScriptCollisionGenerationMethod Method;
+	switch (Meth)
+	{
+	case EVF_GeometryScriptCollisionGenerationMethod::AlignedBoxes:
+		Method = EGeometryScriptCollisionGenerationMethod::AlignedBoxes;
+		break;
+	case EVF_GeometryScriptCollisionGenerationMethod::OrientedBoxes:
+		Method = EGeometryScriptCollisionGenerationMethod::OrientedBoxes;
+		break;
+	case EVF_GeometryScriptCollisionGenerationMethod::MinimalSpheres:
+		Method = EGeometryScriptCollisionGenerationMethod::MinimalSpheres;
+		break;
+	case EVF_GeometryScriptCollisionGenerationMethod::Capsules:
+		Method = EGeometryScriptCollisionGenerationMethod::Capsules;
+		break;
+	case EVF_GeometryScriptCollisionGenerationMethod::ConvexHulls:
+		Method = EGeometryScriptCollisionGenerationMethod::ConvexHulls;
+		break;
+	case EVF_GeometryScriptCollisionGenerationMethod::SweptHulls:
+		Method = EGeometryScriptCollisionGenerationMethod::SweptHulls;
+		break;
+	case EVF_GeometryScriptCollisionGenerationMethod::MinVolumeShapes:
+		Method = EGeometryScriptCollisionGenerationMethod::MinVolumeShapes;
+		break;
+	default:
+		Method = EGeometryScriptCollisionGenerationMethod::ConvexHulls;
+		break;
+	}
+	return Method;
+}
+
+static EGeometryScriptSweptHullAxis Convert(
+	const EVF_GeometryScriptSweptHullAxis &Ax)
+{
+	EGeometryScriptSweptHullAxis Axis;
+	switch (Ax)
+	{
+	case EVF_GeometryScriptSweptHullAxis::X:
+		Axis = EGeometryScriptSweptHullAxis::X;
+		break;
+	case EVF_GeometryScriptSweptHullAxis::Y:
+		Axis = EGeometryScriptSweptHullAxis::Y;
+		break;
+	case EVF_GeometryScriptSweptHullAxis::Z:
+		Axis = EGeometryScriptSweptHullAxis::Z;
+		break;
+	case EVF_GeometryScriptSweptHullAxis::SmallestBoxDimension:
+		Axis = EGeometryScriptSweptHullAxis::SmallestBoxDimension;
+		break;
+	case EVF_GeometryScriptSweptHullAxis::SmallestVolume:
+		Axis = EGeometryScriptSweptHullAxis::SmallestVolume;
+		break;
+	default:
+		Axis = EGeometryScriptSweptHullAxis::Z;
+		break;
+	}
+	return Axis;
+}
+
+static FGeometryScriptCollisionFromMeshOptions Convert(
+	const FVF_GeometryScriptCollisionFromMeshOptions &Options_)
+{
+	FGeometryScriptCollisionFromMeshOptions Options{
+		Options_.bEmitTransaction,
+		Convert(Options_.Method),
+		Options_.bAutoDetectSpheres,
+		Options_.bAutoDetectBoxes,
+		Options_.bAutoDetectCapsules,
+		Options_.MinThickness,
+		Options_.bSimplifyHulls,
+		Options_.ConvexHullTargetFaceCount,
+		Options_.MaxConvexHullsPerMesh,
+		Options_.ConvexDecompositionSearchFactor,
+		Options_.ConvexDecompositionErrorTolerance,
+		Options_.ConvexDecompositionMinPartThickness,
+		Options_.SweptHullSimplifyTolerance,
+		Convert(Options_.SweptHullAxis),
+		Options_.bRemoveFullyContainedShapes,
+		Options_.MaxShapeCount};
+	return Options;
+}
+
+static EGeometryScriptBooleanOperation Convert(
+	const EVF_GeometryScriptBooleanOperation &Operation_)
+{
+	EGeometryScriptBooleanOperation Operation;
+	switch (Operation_)
+	{
+	case EVF_GeometryScriptBooleanOperation::Union:
+		Operation = EGeometryScriptBooleanOperation::Union;
+		break;
+	case EVF_GeometryScriptBooleanOperation::Intersection:
+		Operation = EGeometryScriptBooleanOperation::Intersection;
+		break;
+	case EVF_GeometryScriptBooleanOperation::Subtract:
+		Operation = EGeometryScriptBooleanOperation::Subtract;
+		break;
+	default:
+		Operation = EGeometryScriptBooleanOperation::Subtract;
+		break;
+	}
+	return Operation;
+}
+
+static FGeometryScriptMeshBooleanOptions Convert(
+	const FVF_GeometryScriptMeshBooleanOptions &Options_)
+{
+	FGeometryScriptMeshBooleanOptions Options{
+		Options_.bFillHoles,
+		Options_.bSimplifyOutput,
+		Options_.SimplifyPlanarTolerance};
+	return Options;
+}
+
+static FGeometryScriptCopyMeshFromAssetOptions Convert(
+	const FVF_GeometryScriptCopyMeshFromAssetOptions &Options_)
+{
+	FGeometryScriptCopyMeshFromAssetOptions Options{
+		Options_.bApplyBuildSettings,
+		Options_.bRequestTangents,
+		Options_.bIgnoreRemoveDegenerates};
+	return Options;
+};
+
+static EGeometryScriptLODType Convert(
+	const EVF_GeometryScriptLODType &Type_)
+{
+	EGeometryScriptLODType Type;
+	switch (Type_)
+	{
+	case EVF_GeometryScriptLODType::MaxAvailable:
+		Type = EGeometryScriptLODType::MaxAvailable;
+		break;
+	case EVF_GeometryScriptLODType::HiResSourceModel:
+		Type = EGeometryScriptLODType::HiResSourceModel;
+		break;
+	case EVF_GeometryScriptLODType::SourceModel:
+		Type = EGeometryScriptLODType::SourceModel;
+		break;
+	case EVF_GeometryScriptLODType::RenderData:
+		Type = EGeometryScriptLODType::RenderData;
+		break;
+	default:
+		Type = EGeometryScriptLODType::RenderData;
+		break;
+	}
+	return Type;
+}
+
+static FGeometryScriptMeshReadLOD Convert(
+	const FVF_GeometryScriptMeshReadLOD &LOD_)
+{
+	FGeometryScriptMeshReadLOD LOD{
+		Convert(LOD_.LODType),
+		LOD_.LODIndex};
+	return LOD;
+};
+
+FGeometryScriptMeshSelfUnionOptions Convert(
+	const FVF_GeometryScriptMeshSelfUnionOptions &Options_)
+{
+	FGeometryScriptMeshSelfUnionOptions Options{
+		Options_.bFillHoles,
+		Options_.bTrimFlaps,
+		Options_.bSimplifyOutput,
+		Options_.SimplifyPlanarTolerance,
+		Options_.WindingThreshold};
+	return Options;
+}
+
+EGeometryScriptPrimitivePolygroupMode Convert(
+	const EVF_GeometryScriptPrimitivePolygroupMode &Mode_)
+{
+	EGeometryScriptPrimitivePolygroupMode Mode;
+	switch (Mode_)
+	{
+	case EVF_GeometryScriptPrimitivePolygroupMode::SingleGroup:
+		Mode = EGeometryScriptPrimitivePolygroupMode::SingleGroup;
+		break;
+	case EVF_GeometryScriptPrimitivePolygroupMode::PerFace:
+		Mode = EGeometryScriptPrimitivePolygroupMode::PerFace;
+		break;
+	case EVF_GeometryScriptPrimitivePolygroupMode::PerQuad:
+		Mode = EGeometryScriptPrimitivePolygroupMode::PerQuad;
+		break;
+	default:
+		Mode = EGeometryScriptPrimitivePolygroupMode::PerFace;
+		break;
+	}
+	return Mode;
+}
+
+EGeometryScriptPrimitiveUVMode Convert(
+	const EVF_GeometryScriptPrimitiveUVMode &Mode_)
+{
+	EGeometryScriptPrimitiveUVMode Mode;
+	switch (Mode_)
+	{
+	case EVF_GeometryScriptPrimitiveUVMode::Uniform:
+		Mode = EGeometryScriptPrimitiveUVMode::Uniform;
+		break;
+	case EVF_GeometryScriptPrimitiveUVMode::ScaleToFill:
+		Mode = EGeometryScriptPrimitiveUVMode::ScaleToFill;
+		break;
+	default:
+		Mode = EGeometryScriptPrimitiveUVMode::Uniform;
+		break;
+	}
+	return Mode;
+}
+
+FGeometryScriptPrimitiveOptions Convert(
+	const FVF_GeometryScriptPrimitiveOptions &Options_)
+{
+	FGeometryScriptPrimitiveOptions Options
+	{
+		Convert(Options_.PolygroupMode),
+		Options_.bFlipOrientation,
+		Convert(Options_.UVMode)
+	};
+	return Options;
+}
+
+FGeometryScriptCopyMeshFromComponentOptions Convert(
+	const FVF_GeometryScriptCopyMeshFromComponentOptions &Options_)
+{
+	FGeometryScriptCopyMeshFromComponentOptions Options{
+		Options_.bWantNormals,
+		Options_.bWantTangents,
+		Convert(Options_.RequestedLOD)};
+	return Options;
+}
+
+// 宏 MANUAL_CONVERT 结束
+#endif
+
+UDynamicMesh *UVFGeometryFunctions::SetDynamicMeshCollisionFromMesh(
+	UDynamicMesh *FromDynamicMesh,
+	UDynamicMeshComponent *ToDynamicMeshComponent,
+	FVF_GeometryScriptCollisionFromMeshOptions Options_)
+{
+	FGeometryScriptCollisionFromMeshOptions Options = Convert(Options_);
+
+	return UGeometryScriptLibrary_CollisionFunctions::SetDynamicMeshCollisionFromMesh(
+		FromDynamicMesh,
+		ToDynamicMeshComponent,
+		Options,
+		nullptr);
+}
+
+UDynamicMesh *UVFGeometryFunctions::CopyMeshFromStaticMesh(
+	UStaticMesh *FromStaticMeshAsset,
+	UDynamicMesh *ToDynamicMesh,
+	FVF_GeometryScriptCopyMeshFromAssetOptions AssetOptions,
+	FVF_GeometryScriptMeshReadLOD RequestedLOD)
+{
+	EGeometryScriptOutcomePins Outcome;
+	return UGeometryScriptLibrary_StaticMeshFunctions::CopyMeshFromStaticMesh(
+		FromStaticMeshAsset,
+		ToDynamicMesh,
+		Convert(AssetOptions),
+		Convert(RequestedLOD),
+		Outcome,
+		nullptr);
+}
+
+UDynamicMesh *UVFGeometryFunctions::SetVertexPosition(
+	UDynamicMesh *TargetMesh,
+	int VertexID,
+	FVector NewPosition,
+	bool &bIsValidVertex,
+	bool bDeferChangeNotifications)
+{
+	return UGeometryScriptLibrary_MeshBasicEditFunctions::SetVertexPosition(
+		TargetMesh,
+		VertexID,
+		NewPosition,
+		bIsValidVertex,
+		bDeferChangeNotifications);
+}
+
+UDynamicMesh *UVFGeometryFunctions::ApplyMeshBoolean(
+	UDynamicMesh *TargetMesh,
+	FTransform TargetTransform,
+	UDynamicMesh *ToolMesh,
+	FTransform ToolTransform,
+	EVF_GeometryScriptBooleanOperation Operation,
+	FVF_GeometryScriptMeshBooleanOptions Options)
+{
+	return UGeometryScriptLibrary_MeshBooleanFunctions::ApplyMeshBoolean(
+		TargetMesh,
+		TargetTransform,
+		ToolMesh,
+		ToolTransform,
+		Convert(Operation),
+		Convert(Options),
+		nullptr);
+}
+
+UDynamicMesh *UVFGeometryFunctions::ApplyMeshSelfUnion(
+	UDynamicMesh *TargetMesh,
+	FVF_GeometryScriptMeshSelfUnionOptions Options)
+{
+	return UGeometryScriptLibrary_MeshBooleanFunctions::ApplyMeshSelfUnion(
+		TargetMesh,
+		Convert(Options),
+		nullptr);
+}
+
+UDynamicMesh *UVFGeometryFunctions::AppendFrustum(
+	UDynamicMesh *TargetMesh,
+	FVF_GeometryScriptPrimitiveOptions PrimitiveOptions,
+	float Angle,
+	float AspectRatio,
+	float StartDis,
+	float EndDis)
+{
+	check(TargetMesh);
+	// 实现: 从简单盒上修改点位置制成视锥.
+	// 更好的做法是写一个FMeshShapeGenerator.
+
+	// 生成盒状
+	float DimensionX = 100.0f, DimensionY = 100.0f, DimensionZ = 100.0f;
+	int32 StepsX = 0, StepsY = 0, StepsZ = 0;
+
+	UGeometryScriptLibrary_MeshPrimitiveFunctions::AppendBox(
+		TargetMesh,
+		Convert(PrimitiveOptions),
+		FTransform::Identity,
+		DimensionX,
+		DimensionY,
+		DimensionZ,
+		StepsX,
+		StepsY,
+		StepsZ,
+		EGeometryScriptPrimitiveOriginMode::Center,
+		nullptr);
+
+	// 计算并放置视锥各点的位置
+	TArray<FVector> Positions;
+	{
+		Positions.Reserve(8);
+		auto CalculatePositions = [&](float Distance)
+		{
+			float x = Distance;
+			float y = Distance * tanf(FMath::DegreesToRadians(Angle) / 2);
+			float z = y / AspectRatio;
+			Positions.Push({x, -y, -z});
+			Positions.Push({x, y, -z});
+			Positions.Push({x, y, z});
+			Positions.Push({x, -y, z});
+		};
+		CalculatePositions(StartDis);
+		CalculatePositions(EndDis);
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		bool Success;
+		SetVertexPosition(TargetMesh, i, Positions[i], Success, i != 7);
+	}
+
+	return TargetMesh;
+}
+
+UDynamicMesh *UVFGeometryFunctions::CopyMeshFromComponent(
+	UPrimitiveComponent *Component,
+	UDynamicMesh *ToDynamicMesh,
+	FVF_GeometryScriptCopyMeshFromComponentOptions Options,
+	bool bTransformToWorld)
+{
+	EGeometryScriptOutcomePins Outcome;
+	FTransform LocalToWorld = FTransform::Identity;
+	return UGeometryScriptLibrary_SceneUtilityFunctions::CopyMeshFromComponent(
+		Component,
+		ToDynamicMesh,
+		Convert(Options),
+		bTransformToWorld,
+		LocalToWorld,
+		Outcome,
+		nullptr);
+}
+
+// 宏 !WITHOUT_GEOMETRY_SCRIPT 结束
+#endif
