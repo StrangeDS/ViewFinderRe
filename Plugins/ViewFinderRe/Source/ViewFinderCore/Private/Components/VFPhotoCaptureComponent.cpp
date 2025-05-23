bc1f46f85d416047cd842669b440a72bb9f3d969
StrangeDS <1023221456@qq.com>
2025-05-23 15:02:15 +0800

    UVFPhotoCaptureComponent: 同步场景画面设置

--- Plugins/ViewFinderRe/Source/ViewFinderCore/Private/Components/VFPhotoCaptureComponent.cpp
+++ Plugins/ViewFinderRe/Source/ViewFinderCore/Private/Components/VFPhotoCaptureComponent.cpp
@@ -13,6 +13,10 @@ UVFPhotoCaptureComponent::UVFPhotoCaptureComponent()
 	bCaptureOnMovement = false;
 	bAlwaysPersistRenderingState = false;
 	PrimitiveRenderMode = ESceneCapturePrimitiveRenderMode::PRM_RenderScenePrimitives;
+	PostProcessSettings.bOverride_DynamicGlobalIlluminationMethod = 1;
+	PostProcessSettings.DynamicGlobalIlluminationMethod = EDynamicGlobalIlluminationMethod::Type::Lumen;
+	PostProcessSettings.bOverride_ReflectionMethod = 1;
+	PostProcessSettings.ReflectionMethod = EReflectionMethod::Type::Lumen;
 }
 
 void UVFPhotoCaptureComponent::BeginPlay()
