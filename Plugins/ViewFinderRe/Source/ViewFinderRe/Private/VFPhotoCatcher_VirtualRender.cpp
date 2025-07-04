2c23489881ae16ff7ed5760b27577ce70cb060d6
StrangeDS <1023221456@qq.com>
2025-07-05 03:07:46 +0800

    AVFPhotoCatcher_VirtualRender

---
+++ Plugins/ViewFinderRe/Source/ViewFinderRe/Private/VFPhotoCatcher_VirtualRender.cpp
@@ -0,0 +1,35 @@
+#include "VFPhotoCatcher_VirtualRender.h"
+
+#include "Materials/MaterialParameterCollectionInstance.h"
+
+AVFPhotoCatcher_VirtualRender::AVFPhotoCatcher_VirtualRender()
+    : Super()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void AVFPhotoCatcher_VirtualRender::BeginPlay()
+{
+    Super::BeginPlay();
+
+    MPCI = GetWorld()->GetParameterCollectionInstance(MPC);
+    MPCI->SetScalarParameterValue(TEXT("Radius"), RenderRadius);
+}
+
+void AVFPhotoCatcher_VirtualRender::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    MPCI->SetVectorParameterValue(TEXT("Center"), FLinearColor(GetActorLocation()));
+}
+
+void AVFPhotoCatcher_VirtualRender::DropDown_Implementation()
+{
+    auto PC = PlayerController;
+
+    Super::DropDown_Implementation();
+
+    PlayerController = PC;
+    AddPostProcessToPlayerCamera();
+    PlayerController = nullptr;
+}
---
+++ Plugins/ViewFinderRe/Source/ViewFinderRe/Public/VFPhotoCatcher_VirtualRender.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VFPhotoCatcher_PickUp.h"
+#include "VFPhotoCatcher_VirtualRender.generated.h"
+
+class UMaterialParameterCollection;
+class UMaterialParameterCollectionInstance;
+
+/*
+用于展示: 复刻星际拓荒太空站灯笼的后处理
+向材质参数集更新参数
+丢弃后加上后处理, 然后由Deactivate关闭
+*/
+UCLASS(Blueprintable, ClassGroup = (ViewFinder))
+class VIEWFINDERRE_API AVFPhotoCatcher_VirtualRender : public AVFPhotoCatcher_PickUp
+{
+	GENERATED_BODY()
+
+public:
+	AVFPhotoCatcher_VirtualRender();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	virtual void DropDown_Implementation() override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	TObjectPtr<UMaterialParameterCollection> MPC;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	float RenderRadius = 1000.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	TObjectPtr<UMaterialParameterCollectionInstance> MPCI;
+};
